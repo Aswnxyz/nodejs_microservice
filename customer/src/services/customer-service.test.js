f361c8cc89eca89591397b3043852a55990e484d
Aswin <aswinanith007@gmail.com>
2023-11-18 19:38:27 +0530

    Added test cases confiugurations

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,9 @@
+//which service it is 
+describe("CustomerService",()=>{
+    //which function
+    describe("SignIn",()=>{
+        //Which Scenario we are testing
+        test("Validate user inputs",async()=>{}); 
+        test("Validate response",async ()=>{}); 
+    })
+})
